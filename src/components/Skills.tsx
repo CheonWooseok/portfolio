e962f8df2ef3e084b8b6f255bdb7e1d96a2bf55a
wooseok <rightsn1110@gmail.com>
2024-08-16 08:07:17 +0900

    skills divide

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -4,18 +4,41 @@ import { SectionTitle, SectionWrapper } from "../App";
 import AntdIcon from "../assets/icons/ant-design.svg";
 import AWSIcon from "../assets/icons/aws.svg";
 import DynamoDBIcon from "../assets/icons/dynamodb.svg";
+import LambdaIcon from "../assets/icons/lambda.svg";
+import MariaDBIcon from "../assets/icons/mariadb.svg";
 import MuiIcon from "../assets/icons/material-ui.svg";
 import NestjsIcon from "../assets/icons/nestjs.svg";
 import ReactIcon from "../assets/icons/react.svg";
 import ReduxIcon from "../assets/icons/redux.svg";
 import SwrIcon from "../assets/icons/swr.png";
-import TypescriptIcon from "../assets/icons/typscript.png";
+// import TypescriptIcon from "../assets/icons/typscript.png";
 
 const Skills = forwardRef<HTMLDivElement>((props, ref) => {
+	const SkillWrapper = styled.div`
+		display: flex;
+		/* justify-content: center; */
+		align-items: center;
+		flex-wrap: wrap;
+	`;
+
 	const Skill = styled.img`
 		width: 80px;
 		height: 80px;
 		margin: 10px;
+	`;
+
+	const SkillTitle = styled.h2`
+		font-size: 1.7rem;
+		font-weight: 700;
+
+		&:before {
+			content: "";
+			background-color: #f4ce14;
+			height: 0.8em;
+			width: 3px;
+			display: inline-block;
+			margin-right: 10px;
+		}
 	`;
 
 	return (
@@ -25,24 +48,32 @@ const Skills = forwardRef<HTMLDivElement>((props, ref) => {
 				style={{
 					backgroundColor: "white",
 					width: "80%",
-					minHeight: "50vh",
+					// minHeight: "50vh",
 					borderRadius: "10px",
 					padding: "20px",
 					marginTop: "50px",
 					boxShadow: "0 0 10px 0 rgba(0, 0, 0, 0.1)",
 					display: "flex",
-					flexWrap: "wrap",
+					flexDirection: "column",
 				}}
 			>
-				<Skill src={TypescriptIcon} alt="typescript" />
-				<Skill src={NestjsIcon} alt="nestjs" />
-				<Skill src={AWSIcon} alt="aws" />
-				<Skill src={ReactIcon} alt="react" />
-				<Skill src={AntdIcon} alt="antd" />
-				<Skill src={MuiIcon} alt="mui" />
-				<Skill src={SwrIcon} alt="swr" />
-				<Skill src={ReduxIcon} alt="redux" />
-				<Skill src={DynamoDBIcon} alt="dynamodb" />
+				<SkillTitle>Frontend</SkillTitle>
+				<SkillWrapper>
+					<Skill src={ReactIcon} alt="react" />
+					<Skill src={AntdIcon} alt="antd" />
+					<Skill src={MuiIcon} alt="mui" />
+					<Skill src={SwrIcon} alt="swr" />
+					<Skill src={ReduxIcon} alt="redux" />
+				</SkillWrapper>
+				<SkillTitle>Backend</SkillTitle>
+				<SkillWrapper>
+					{/* <Skill src={TypescriptIcon} alt="typescript" /> */}
+					<Skill src={NestjsIcon} alt="nestjs" />
+					<Skill src={AWSIcon} alt="aws" />
+					<Skill src={LambdaIcon} alt="lambda" />
+					<Skill src={DynamoDBIcon} alt="dynamodb" />
+					<Skill src={MariaDBIcon} alt="mariadb" />
+				</SkillWrapper>
 			</div>
 		</SectionWrapper>
 	);
